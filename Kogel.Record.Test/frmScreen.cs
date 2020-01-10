f2eec6943b8642b77808b14a18c70bb0ac13bb1c
侨文 袁 <935368322@qq.com>
2020-01-10 09:16:07 +0800

    屏幕录制扩展

--- Kogel.Record.Test/frmScreen.cs
+++ Kogel.Record.Test/frmScreen.cs
@@ -1,5 +1,7 @@
 ﻿using AForge.Video;
+using AForge.Video.FFMPEG;
 using Kogel.Record.Extension;
+using Kogel.Record.Interfaces;
 using System;
 using System.Collections.Generic;
 using System.ComponentModel;
@@ -14,14 +16,14 @@ namespace Kogel.Record.Test
 {
 	public partial class frmScreen : Form
 	{
-		private ScreenRecorder recorder { get; set; }
+		private IRecorder recorder { get; set; }
 		private string recorderPath { get; set; }
-	 	public frmScreen()
+		public frmScreen()
 		{
 			InitializeComponent();
 			//初始化录制器
 			recorderPath = AppDomain.CurrentDomain.BaseDirectory + DateTime.Now.ToString("MMddHHmmss") + ".avi";
-			recorder = new ScreenRecorder(recorderPath, 10, true);
+			recorder = new ScreenRecorderExtension(recorderPath, 10, true);
 		}
 
 		/// <summary>
@@ -73,6 +75,31 @@ namespace Kogel.Record.Test
 		private void btnOpenCamera_Click(object sender, EventArgs e)
 		{
 			new frmCamera().Show();
+		}
+	}
+
+	/// <summary>
+	/// 屏幕录制扩展
+	/// </summary>
+	public class ScreenRecorderExtension : ScreenRecorder
+	{
+		public ScreenRecorderExtension(string aviFilePath, int defaultFrameRate = 10, bool isLoopingWav = false, VideoCodec videoCodec = VideoCodec.MSMPEG4v2) :
+			base(aviFilePath, defaultFrameRate, isLoopingWav, videoCodec)
+		{
+
+		}
+
+		//鼠标颜色
+		Pen mousePen = new Pen(new SolidBrush(Color.Black));
+		protected override void VideoStreamer_NewFrame(object sender, NewFrameEventArgs eventArgs)
+		{
+			//绘制鼠标
+			Bitmap bitmap = eventArgs.Frame;//得到鼠标后的图
+			using (Graphics graphics = Graphics.FromImage(bitmap))
+			{
+				graphics.DrawEllipse(mousePen, new Rectangle(Control.MousePosition.X, Control.MousePosition.Y, 10, 10));
+			}
+			base.VideoStreamer_NewFrame(sender, new NewFrameEventArgs(bitmap));
 		}
 	}
 }
